e51edd43336a39d8cff79f9a85b1eaedcbeb96b1
Vaishali <aggarwal.vaishali785@gmail.com>
2024-09-17 20:25:03 +0530

    parent page completed

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,5 +1,24 @@
-const Page = () => {
-	return <div className="">Page</div>
+import Announcements from "@/app/components/Announcements"
+import BigCalendar from "@/app/components/BigCalendar"
+import EventCalendar from "@/app/components/EventCalendar"
+
+const ParentPage = () => {
+	return (
+		<div className="flex flex-col xl:flex-row gap-4 p-4">
+			{/* left */}
+			<div className="w-full xl:w-2/3 ">
+				<div className="h-full bg-white p-4 rounded-md">
+					<h1 className="text-xl font-semibold"> Schedule (John Doe)</h1>
+					<BigCalendar />
+				</div>
+			</div>
+			{/* right */}
+			<div className="w-full xl:w-1/3 flex flex-col gap-8 ">
+				<EventCalendar />
+				<Announcements />
+			</div>
+		</div>
+	)
 }
 
-export default Page
+export default ParentPage
